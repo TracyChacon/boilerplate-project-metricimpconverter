09c449bea5f1482f2d8bd9ef1e11b0d91b70eec5
Tracy Chacon <tracy.chacon.00@gmail.com>
2024-10-31 20:25:30 -0500

    add: test, read a decimal num input

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -8,4 +8,10 @@ suite('Unit Tests', function () {
   test('should correctly read a whole number input', function () {
     assert(convertHandler.getNum(55)).to.be.a('number')
   })
+
+  test('should correctly read a decimal number input', function () {
+    assert.equal(convertHandler.getNum(4.5), 4.5)
+
+    done()
+  })
 })
