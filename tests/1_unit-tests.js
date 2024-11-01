0447b8c10f36201a8a62925a382d998f918936fa
Tracy Chacon <tracy.chacon.00@gmail.com>
2024-10-31 21:00:40 -0500

    add: test convert gal to L and vice versa

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -12,4 +12,9 @@ suite('Unit Tests', function () {
   test('should correctly read a decimal number input', function () {
     assert.equal(convertHandler.getNum('4.5km'), 4.5)
   })
+
+  test('can convert gal to L and vice versa', function () {
+    assert.equal(convertHandler.getReturnUnit('gal'), 'L')
+    assert.equal(convertHandler.getReturnUnit('L'), 'gal')
+  })
 })
